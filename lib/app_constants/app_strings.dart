9bba369631dcc9f1b5fd986a0734c98600d03051
chintank8080 <chintank8080@gmail.com>
2024-05-01 19:03:31 +0530

    ceated app constant files

---
+++ lib/app_constants/app_strings.dart
@@ -0,0 +1,17 @@
+class AppStrings {
+  static const welcomeLine = "Hi, Welcome Back! \u{1F44B}";
+  static const password = "Password";
+  static const passwordHint = "Please Enter Your Password";
+  static const email = "Email";
+  static const emailHint = "Please Enter Your Email";
+  static const remeberMe = "Remember Me";
+  static const login = "Login";
+  static const immence = "immence";
+  static const name = "Name";
+  static const phone = "Phone Number";
+  static const phoneHint = "Enter your phone number";
+  static const emailAddress = "Email Address";
+  static const emailAddressHint = "Enter your email";
+  static const signUp = "Sign Up";
+  static const dontHaveAnAccount = "Don't have an account?";
+}
---
+++ lib/app_constants/app_text_style.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:immence_task/app_constants/app_colors.dart';
+
+class AppTextStyle {
+  static const hintStyle = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.hintTextColor,
+  );
+  static const labelStyle = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.hintTextColor,
+  );
+  static const buttonText = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+  );
+  static const headingText = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+    color: AppColors.black,
+  );
+
+  static const titleText = TextStyle(
+    fontFamily: "Manrope",
+    fontSize: 26,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryColor,
+  );
+}
